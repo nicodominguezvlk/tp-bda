8d8a71bf1868bd0a69663fb37835b70db3f74a4e
vale.lascano <vale.lascano@hotmail.com>
2023-11-12 23:15:44 -0300

    error delete

--- EstacionService/demo/src/main/java/com/abnimavade/tp/repositories/EstacionRepository.java
+++
@@ -1,9 +0,0 @@
-package com.abnimavade.tp.repositories;
-
-import com.abnimavade.tp.models.Estacion;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface EstacionRepository extends JpaRepository<Estacion, Long> {
-}
