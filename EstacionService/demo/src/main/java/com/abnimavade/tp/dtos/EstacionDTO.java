1258de6152ec9042794e88da47b2073ec7a5f517
vale.lascano <vale.lascano@hotmail.com>
2023-11-12 23:52:40 -0300

    alquiler svc y estacion svc SIN APIGW

---
+++ EstacionService/demo/src/main/java/com/abnimavade/tp/dtos/EstacionDTO.java
@@ -0,0 +1,19 @@
+package com.abnimavade.tp.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class EstacionDTO {
+    private long estacionId;
+    private String nombre;
+    private Date fechaHoraCreacion;
+    private double latitud;
+    private double longitud;
+}
---
+++ EstacionService/demo/src/test/java/com/abnimavade/tp/AlquilerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abnimavade.tp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlquilerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
