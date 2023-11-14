3e95f3df77aad1a88e20d620fdd9aa3417cc4c77
JyrkiMakinen <nic.dom2002@gmail.com>
2023-11-14 12:28:35 -0300

    CotizacionesService

---
+++ alquileres/src/main/java/com/abnimavade/alquileres/services/CotizacionesService.java
@@ -0,0 +1,37 @@
+package com.abnimavade.alquileres.services;
+
+import org.springframework.http.*;
+import org.springframework.web.client.RestTemplate;
+
+public class CotizacionesService {
+
+    private final String apiUrl = "http://34.82.105.125:8080/convertir";
+
+    public double convertirMoneda(String moneda, double cantidadArs){
+        // Set the request headers
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+
+        // Create the request body
+        String requestBody = String.format("{\"moneda_destino\":\"%s\",\"importe\":%f}", moneda, cantidadArs);
+
+        // Create the request entity with headers and body
+        HttpEntity<String> requestEntity = new HttpEntity<>(requestBody, headers);
+
+        // Create a RestTemplate
+        RestTemplate restTemplate = new RestTemplate();
+
+        // Make the HTTP request
+        ResponseEntity<String> responseEntity = restTemplate.exchange(apiUrl, HttpMethod.POST, requestEntity, String.class);
+
+        // Parse the response body to extract the converted currency value
+        String responseBody = responseEntity.getBody();
+        // Assuming your response always has "importe" as a key
+        double convertedValue;
+        assert responseBody != null;
+        convertedValue = Double.parseDouble(responseBody.split(":")[1].replaceAll("[^\\d.]", ""));
+
+        return convertedValue;
+    }
+
+}
