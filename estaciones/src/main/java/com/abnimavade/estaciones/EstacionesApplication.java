40323de329ac2bae4eb2de1c21b916f3036b0c1d
JyrkiMakinen <nic.dom2002@gmail.com>
2023-11-13 21:57:01 -0300

    Creacion de estructura de microservicios
    Toda la lógica está ahora en el proyecto gateway

---
+++ estaciones/src/main/java/com/abnimavade/estaciones/EstacionesApplication.java
@@ -0,0 +1,13 @@
+package com.abnimavade.estaciones;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstacionesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstacionesApplication.class, args);
+	}
+
+}
---
+++ estaciones/src/test/java/com/abnimavade/estaciones/EstacionesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abnimavade.estaciones;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstacionesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
